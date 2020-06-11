2256f92c7c000223024124bc20a03b164dcce34e
denacho <55347598+denacho@users.noreply.github.com>
2020-06-11 13:39:52 +0200

    root added

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Cooking Mama',
+    description: ' learn to cook',
+    keywords: 'cooking, video online, online course, chef, recipe ideas',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
